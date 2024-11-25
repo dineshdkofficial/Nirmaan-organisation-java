c61de15c65ec2e8599e97dd3d230f11298741457
dineshdkofficial <dineshdk9725@gmail.com>
2024-11-25 13:27:04 +0530

    day 3

---
+++ variabletask.java
@@ -0,0 +1,26 @@
+public class variabletask
+{
+  public static void main(String[]args){
+    byte a=123;
+    short b=12345;
+    long c=1234567890l;
+    int d=123456789;
+    float e=2.20f;
+    double f=13.123456;
+    char name='d';
+    Boolean statement=true;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(name);
+System.out.println(statement);
+
+}
+}
+
+
+
+
---
+++ variabletask2.java
@@ -0,0 +1,9 @@
+public class variabletask2
+{
+  public static void main(String[]args){
+   int firstnumber=20;
+  int secondnumber=30;
+  int thirdnumber=40;
+System.out.println("a+b+c"+(firstnumber+secondnumber+thirdnumber));
+}
+}
