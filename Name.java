e700d7867428ddce7e74fada2eaf8e5688423936
dineshdkofficial <dineshdk9725@gmail.com>
2024-11-28 12:29:25 +0530

    day4

---
+++ Name.java
@@ -0,0 +1,31 @@
+package Day4;
+
+public class Name {
+	public static void main(String[]agrs) {
+//		for(int i=1;i<=100;i++) {
+//			if(i%3==0) {
+//				System.out.println("dinesh");
+//				
+//			}else {
+//				System.out.println(i);
+//			}
+//		}
+//		for(int i=1;i<=100;i++) {
+//			if(!(i%2==0)) {
+//				System.out.println("odd");
+//				
+//			}else {
+//				System.out.println(i);
+//			}
+//		}
+		for(int i=1;i<=100;i++) {
+			if(i%2==0) {
+				System.out.println("Even");
+				
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
---
+++ Nestedforloop.java
@@ -0,0 +1,13 @@
+package Day4;
+
+public class Nestedforloop {
+	public static void main(String[]agrs) {
+		for (int j=1;j<=10;j++) {
+			for (int num=1;num<=5;num++) {
+				System.out.println(num);
+			}
+			System.out.println("single Iteration end");
+		}
+	}
+
+}
