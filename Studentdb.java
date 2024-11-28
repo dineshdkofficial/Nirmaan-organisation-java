6cc90e9a9be9d3a449ad3a2451b87e7c44cb1ccd
dineshdkofficial <dineshdk9725@gmail.com>
2024-11-28 13:08:09 +0530

    day 5
    part 2

---
+++ Studentdb.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class Studentdb {
+	String Studentname="";
+	int Age;
+	int RollNo;
+	String DateOfbrith="";
+public static void main(String[] args) {
+	Studentdb s1=new Studentdb();
+	s1.Studentname="Dinesh";
+    s1.Age=22;
+   s1.RollNo=1312;
+   s1.DateOfbrith="14/11/2002";
+   System.out.println(s1.Studentname);
+   System.out.println(s1.Age);
+   System.out.println(s1.RollNo);
+   System.out.println(s1.DateOfbrith);
+   
+}		
+}
+
