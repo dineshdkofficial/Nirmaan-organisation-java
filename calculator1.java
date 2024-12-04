85b034cb22720be004ae70470b91416c3b5a4f20
dineshdkofficial <dineshdk9725@gmail.com>
2024-12-04 11:55:17 +0530

    day9

---
+++ calculator1.java
@@ -0,0 +1,19 @@
+package day9;
+
+public class calculator1 {
+	
+	int Addition(int a,int b) {
+		return a+b;
+	}
+	int subtraction(int a,int b) {
+		return a-b;
+
+}
+	int multiplication(int a,int b) {
+		return a*b;
+	}
+	int Division(int a,int b) {
+		return a/b;
+
+	}
+}
---
+++ calculator2.java
@@ -0,0 +1,20 @@
+package day9;
+
+public class calculator2 {
+	public static void main(String[] args) {
+		calculator1 calc=new calculator1();
+				int Addition=calc.Addition(20,10);
+			System.out.println(Addition);
+			
+			int subtraction=calc.subtraction(20,10);
+			System.out.println(subtraction);
+			
+			int multiplication=calc.multiplication(20,10);
+			System.out.println(multiplication);
+			
+			int Division=calc.Division(20,10);
+			System.out.println(Division);
+				
+	}
+
+}
