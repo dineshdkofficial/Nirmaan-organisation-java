bf773b923c8e1d88f463a747193eb6ab21fea391
dineshdkofficial <dineshdk9725@gmail.com>
2024-12-06 11:27:28 +0530

    day8

---
+++ Asssement1.java
@@ -0,0 +1,126 @@
+package day8;
+import java.util.Scanner;
+public class Asssement1 {
+	//Q1 write ajava program to cast vote if age is greater than 18?
+//	void vote(int age) {
+//		if(age>=18) {
+//			System.out.println("you are eligible to vote");
+//		}else {
+//			System.out.println("you are not eligible to vote");
+//		}
+//	}
+
+	
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);
+//		Asssement1 a=new Asssement1();
+//		System.out.println("Enter your age");
+//		int age =sc.nextInt();
+//		a.vote(age);
+	
+//	Q2
+//		public static void main(String[] args) {
+//			for (int i=1;i<=100;i++) {
+//				if(i% 4==0) {
+//					System.out.println("jack");
+//					
+//				}else
+//				{
+//					System.out.println(i);
+//				}
+//			}
+//		}
+//	}
+// Q3
+//	public static void main(String[] args) {
+//		for(int i=1;i<=70;i++) {
+//			if(i%2!=0)
+//			{
+//				continue;
+//			}
+//			else {
+//				System.out.println(i);
+//			
+//			}
+//			}
+//		}
+//	}
+//	Q4
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("Enter your number");
+//		int num =sc.nextInt();
+//		if(num % 2 ==0)
+//{
+//			System.out.println("num is even");}
+//			else {
+//				System.out.println("num is odd");
+//			}
+//}
+//	}
+//		
+//	Q5
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("Enter your name");
+//		String name=sc.nextLine();
+//		System.out.println(name.toUpperCase());
+//		System.out.println("Enter your name2");
+//		String name2=sc.nextLine();
+//		System.out.println(name2.tolowerCase());
+//		
+//		
+//		
+//	}
+//		
+//	}
+//	Q6
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);	
+//		System.out.println("Enter your mark");
+//		int mark=sc.nextInt();
+//		if(mark>=35) {
+//			System.out.println("pass");
+//		}
+//		
+//		else {
+//		System.out.println("fail");
+//	}
+//		}
+//}
+
+//	Q7
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);	
+//		System.out.println("Enter the number of subjects:");
+//		int totalsubject=sc.nextInt();
+//		float totalMarks=0;
+//		
+//			System.out.println("Enter marks for subject:");
+//			for (int i =0;i <totalsubject;i++) { 
+//			totalMarks +=sc.nextFloat();
+//			
+//			}
+//		float averageMarks =totalMarks/totalsubject;
+//		System.out.println("Total Marks:"+totalMarks);
+//		System.out.println("averageMarks:"+averageMarks);
+//		}
+//}
+//	Q8
+//	public static void main(String[] args) {
+//	Scanner sc =new Scanner(System.in);	
+//		System.out.println("Enter your year");
+//		int year=sc.nextInt();
+//		if(year % 4==0) {
+//			System.out.println("Leap year");
+//		}else {
+//			System.out.println("Is not Leap year");
+//		}
+//	}
+
+	
+}
+		
+	
+	
+
