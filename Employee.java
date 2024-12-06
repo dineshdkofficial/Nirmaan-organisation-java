df9a14a712557dd416b9b1ef736dd1fe768de023
dineshdkofficial <dineshdk9725@gmail.com>
2024-12-06 11:32:26 +0530

    day11

---
+++ Employee.java
@@ -0,0 +1,48 @@
+package day11;
+
+public class Employee {
+	
+ private String empName;
+ private int empAge;
+ private long empPhoneNo;
+ private String empDepartment;
+ 
+ public Employee() {
+	 
+ }
+ public Employee(String empName,int empAge,long empPhoneNo,String empDepartment) {
+	 this.empName=empName;
+	 this.empAge=empAge;
+	 this.empPhoneNo=empPhoneNo;
+	 this.empDepartment= empDepartment;
+ }
+ public void setEmpName(String empName) {
+	 this.empName=empName;
+	 
+ }
+ public void setEmpAge(int empAge) {
+	 this.empAge=empAge;
+ }
+ public void setEmpPhoneNo(long empPhoneNo) {
+	 this.empPhoneNo=empPhoneNo;
+ }
+ public void setEmpDepartment(String empDepartment) {
+	 this.empDepartment= empDepartment;
+	  }
+
+ public String getEmpName() {
+	  return empName;
+ }
+ public int getEmpAge() {
+	  return empAge;
+}
+ public long getEmpPhoneNo() {
+	  return empPhoneNo;
+}
+ public String getEmpDepartment() {
+	  return empDepartment;
+}
+ public String toString() {
+	 return "your Name:"+empName+"\n your empAge :"+empAge+"\n your empPhoneNo :"+ empPhoneNo+"\n your empDepartment :"+empDepartment;
+ }
+}
--- main.java
+++ main.java
@@ -1,15 +1,23 @@
-package day10;
+package day11;
 
 public class main {
-	public static void main(String[]agrs) {
-		Car car1=new Car();
-		car1.displayCarDetails();
-		
-		Car car2=new Car("tata");
-		car2.displayCarDetails();
-		
-		Car car3=new Car("tata",2000);
-		car3.displayCarDetails();
+	public static void main(String[] args) {
+	Employee emp =new Employee("Dinesh",22,700219725l,"IT");
+			System.out.println(emp.getEmpName());
+	System.out.println(emp.getEmpAge());
+	System.out.println(emp.getEmpPhoneNo());
+	System.out.println(emp.getEmpDepartment());
+	
+	System.out.println(emp);
+	
+	Employee emp1 =new Employee();
+	emp1.setEmpName("kumar");
+	emp1.setEmpAge(20);
+	emp1.setEmpPhoneNo(207856963662l);
+	emp1.setEmpDepartment("java");
+	
+	
+	
+			
 	}
 }
-
