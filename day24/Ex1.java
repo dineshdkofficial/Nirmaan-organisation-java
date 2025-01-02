a67083259bbbdea6b2ca4ec04e12a402439c4f92
dineshdkofficial <dineshdk9725@gmail.com>
2025-01-02 11:55:30 +0530

    day24

---
+++ day24/Ex1.java
@@ -0,0 +1,54 @@
+package day24;
+
+public class Ex1 {
+	public static void main(String[] args) {
+//		task1
+//	int sum=0;	
+//	int count =0;
+//			for (int i=1;i<=100;i++) {
+//			if(i%2!=0) {
+//				count ++;
+//				sum +=i;
+//			
+//				}
+//			}
+//		
+//			System.out.println(sum);
+//		System.out.println(count);
+		
+//		task2
+//		int sum=0;	
+//		int count =0;
+//				for (int i=1;i<=100;i++) {
+//				if(i%2==0) {
+//					count ++;
+//					sum +=i;
+//				
+//					}
+//				}
+//			
+//				System.out.println(sum);
+//				System.out.println(count);
+//		
+		
+//		task3
+//		String name ="Dinesh";
+//    for(char let : name.toCharArray() ) {
+//    	System.out.println(let);
+//    	for(int i=name.length()-1;i>=0;i--) {
+//    	System.out.print(name.charAt(i));
+//    	
+//    }
+		
+//		task4
+		int num=5;
+		int factorial=1;
+		for(int i=1;i<=num;i++) {
+			factorial*=i;
+			
+		}
+		System.out.println("factorial ="+num);
+	}
+}
+		
+
